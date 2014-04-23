d9b573455a5dc09030efe3dff93370f92797e074
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-04-23 18:09:34 +0400

    [config] Fix exception message

--- config/io/reader/PhpFileReader.php
+++ config/io/reader/PhpFileReader.php
@@ -117,7 +117,13 @@ class PhpFileReader implements IReader, ILocalizable, IConfigAliasResolverAware,
                         $masterValue->set($localValue, IConfigValue::KEY_LOCAL)
                             ->save();
                     } catch (InvalidArgumentException $e) {
-                        throw new UnexpectedValueException('Local property "{key}" should be scalar.', 0, $e);
+                        throw new UnexpectedValueException($this->translate(
+                                'Local property "{key}" should be scalar.',
+                                ['key' => $key]
+                            ),
+                            0,
+                            $e
+                        );
                     }
                 } else {
                     throw new UnexpectedValueException($this->translate(
