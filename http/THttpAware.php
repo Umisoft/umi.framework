4c6b7610f2153cf087a3a16e236ec120ff5c28bc
Daria Prusova <guzhova@umisoft.ru>
2014-04-27 20:59:40 +0400

    [http] get request as single instance

--- http/THttpAware.php
+++ http/THttpAware.php
@@ -36,7 +36,7 @@ trait THttpAware
     protected function createHttpRequest()
     {
         return $this->getHttpFactory()
-            ->createRequest();
+            ->getRequest();
     }
 
     /**
